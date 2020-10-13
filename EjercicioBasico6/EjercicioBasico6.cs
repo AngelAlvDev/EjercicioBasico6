b555db1aef6e7b5265d153836ca77b5e441191fb
AngelAlvDev <angelalv.alumn@cescristorey.com>
2020-10-13 10:49:28 +0200

    Ejercicio 6 finalizado usando foreach

---
+++ EjercicioBasico6/EjercicioBasico6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioBasico6
+{
+    class EjercicioBasico6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca el numero a analizar: ");
+            String cadena = Console.ReadLine();
+            Int16 sietes = 0;
+            foreach(Char numero in cadena){
+                if (numero == '7') {
+                    sietes++;
+                }
+            }
+            Console.WriteLine("El número de 7 introducido es " + sietes);
+        }
+    }
+}
